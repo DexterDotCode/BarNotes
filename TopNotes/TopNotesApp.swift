a694fec35ab6ec98eafe6a79bbad6b8cdad1606c
Dexter.Code <dextercode78004@gmail.com>
2025-03-27 00:12:43 +0530

    Initial commit

---
+++ TopNotes/TopNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopNotesApp.swift
+//  TopNotes
+//
+//  Created by Tanish Mittal on 26/03/25.
+//
+
+import SwiftUI
+
+@main
+struct TopNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
